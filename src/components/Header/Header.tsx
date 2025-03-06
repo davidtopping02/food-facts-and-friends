5218c32e74e361bed0722285dc3a38c18fc9d017
David Topping <71976336+davidtopping02@users.noreply.github.com>
2025-03-06 18:54:40 +0000

    added deployment action

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,20 +6,20 @@ import "./Header.css";
 
 const Header: React.FC = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col d-flex align-items-center justify-content-center">
-          <Link
-            to="/"
-            className="d-flex align-items-center text-decoration-none text-dark"
-            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-          >
-            <img src={logo} alt="Logo" className="logo me-3" />
-            <h1 className="mb-0">Food Facts and Friends</h1>
-          </Link>
-        </div>
+    // <div className="container">
+    <div className="row">
+      <div className="col d-flex align-items-center justify-content-center">
+        <Link
+          to="/"
+          className="d-flex align-items-center text-decoration-none text-dark"
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        >
+          <img src={logo} alt="Logo" className="logo me-3" />
+          <h1 className="mb-0">Food Facts and Friends</h1>
+        </Link>
       </div>
     </div>
+    // </div>
   );
 };
 
