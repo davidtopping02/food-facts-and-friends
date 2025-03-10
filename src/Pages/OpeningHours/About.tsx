5f8d9d3f446be29bc2e92abc018b51182431770b
David Topping <71976336+davidtopping02@users.noreply.github.com>
2025-03-10 16:33:18 +0000

    added all required pages

---
+++ src/Pages/OpeningHours/About.tsx
@@ -0,0 +1,10 @@
+function OpeningHours() {
+  return (
+    <div className="container mt-5">
+      <h1>OpeningHours Page</h1>
+      <p>Welcome to the OpeningHours page!</p>
+    </div>
+  );
+}
+
+export default OpeningHours;
