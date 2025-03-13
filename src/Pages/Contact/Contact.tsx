6cd9a9b52126d222af6553a05bb57466a2286b98
Hannah Mowbray <hannahmowbray3@gmail.com>
2025-03-13 19:03:15 +0000

    added contact form
    Signed-off-by: Hannah Mowbray <hannahmowbray3@gmail.com>

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,6 +1,7 @@
 import { useSpring, animated } from "react-spring";
-import { contactInfoCardText } from "./ContactText";
-import { Envelope, TelephoneFill } from "react-bootstrap-icons";
+
+import { ContactForm } from "./ContactForm";
+import { ContactInfo } from "./ContactInfo";
 
 function Contact() {
   const fadeIn = useSpring({
@@ -12,42 +13,23 @@ function Contact() {
 
   return (
     <>
-      <div className="container mt-5">
-        <animated.div className="row" style={fadeIn}>
-          <h1 className="heading heading-1">Contact Page</h1>
-        </animated.div>
-      </div>
+      {/* <div className="container mt-5 p-0 m-0"> */}
+      <animated.div className="row" style={fadeIn}>
+        <h1 className="heading heading-1">Contact Page</h1>
+      </animated.div>
+      {/* </div>s */}
 
-      <div className="row justify-content-center">
-        <div className="col-lg-4 col-md-6 col-sm-12 mb-4">
-          <div className="card w-200 h-200">
-            <div className="card-body text-center d-flex flex-column justify-content-center">
-              <h2 className="heading heading-2">{contactInfoCardText.name}</h2>
-              <h3 className="heading heading-3">{contactInfoCardText.role}</h3>
-              <div>
-                <div className="row">
-                  <Envelope className="col align-self-center" />
-                  <p className="col align-self-center">
-                    {contactInfoCardText.email}
-                  </p>
-                </div>
-                <div className="row">
-                  <TelephoneFill className="col align-self-center" />
-                  <p className="col align-self-center">
-                    {contactInfoCardText.primaryPhone}
-                  </p>
-                </div>
-                <div className="row">
-                  <TelephoneFill className="col align-self-center" />
-                  <p className="col align-self-center">
-                    {contactInfoCardText.secondaryPhone}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+      {/* <div className="container mt-5 p-0 m-0"> */}
+      <div className="row">
+        <div className="col-sm-6">
+          <ContactForm />
+        </div>
+
+        <div className="col-sm-6">
+          <ContactInfo />
         </div>
       </div>
+      {/* </div> */}
     </>
   );
 }
