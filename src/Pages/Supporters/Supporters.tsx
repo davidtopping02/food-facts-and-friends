c64869daab8055c5fa37340813ddae672dd401cc
David Topping <71976336+davidtopping02@users.noreply.github.com>
2025-03-13 20:11:55 +0000

    formatting is oooogly but wil update in due course

--- src/Pages/Supporters/Supporters.tsx
+++ src/Pages/Supporters/Supporters.tsx
@@ -1,8 +1,26 @@
 function Supporters() {
+  const logos = Object.values(
+    import.meta.glob("/src/assets/supporters-logos/*.png", { eager: true })
+  ) as { default: string }[];
+
   return (
     <div className="container mt-5">
-      <h1 className="heading heading-1">Supporters Page</h1>
-      <p>Welcome to the Supporters page!</p>
+      <div className="row">
+        {logos.map((logo, index) => (
+          <div
+            key={index}
+            className="col-6 col-md-4 col-lg-3 mb-4 d-flex justify-content-center"
+          >
+            <div className="logo-container">
+              <img
+                src={logo.default}
+                alt={`Supporter Logo ${index + 1}`}
+                className="img-fluid logo-image"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
