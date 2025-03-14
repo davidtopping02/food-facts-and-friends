24737eb3ee81fdeee3ff5235d2c11a0a138269b7
David Topping <71976336+davidtopping02@users.noreply.github.com>
2025-03-14 16:41:01 +0000

    updated supporter page

--- src/Pages/Supporters/Supporters.tsx
+++ src/Pages/Supporters/Supporters.tsx
@@ -1,26 +1,14 @@
+import SupportersGallery from "../../Components/SupportersGallery/SupportersGallery";
+
 function Supporters() {
   const logos = Object.values(
     import.meta.glob("/src/assets/supporters-logos/*.png", { eager: true })
   ) as { default: string }[];
 
   return (
-    <div className="container mt-5">
-      <div className="row">
-        {logos.map((logo, index) => (
-          <div
-            key={index}
-            className="col-6 col-md-4 col-lg-3 mb-4 d-flex justify-content-center"
-          >
-            <div className="logo-container">
-              <img
-                src={logo.default}
-                alt={`Supporter Logo ${index + 1}`}
-                className="img-fluid logo-image"
-              />
-            </div>
-          </div>
-        ))}
-      </div>
+    <div className="mt-5">
+      <h2 className="heading page-title">Our Supporters Gallery</h2>
+      <SupportersGallery logos={logos} />
     </div>
   );
 }
