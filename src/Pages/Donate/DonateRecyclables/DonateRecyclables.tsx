95d02ab3a9ef26a7a8469fd46a4eaaeeb9c8f32f
David Topping <71976336+davidtopping02@users.noreply.github.com>
2025-03-19 18:14:39 +0000

    added all relevant donate pages

--- src/Pages/Donate/DonateRecyclables/DonateRecyclables.tsx
+++ src/Pages/Donate/DonateRecyclables/DonateRecyclables.tsx
@@ -1,12 +1,29 @@
 import React from "react";
+import { Container, Card } from "react-bootstrap";
+import recyclablesImage from "../../../assets/images/items_for_good_causes.png";
 
-const DonateRecyclables: React.FC = () => {
-  return (
-    <div>
-      <h1>Donate Recyclables</h1>
-      <p>Your contributions help us make a difference!</p>
-    </div>
-  );
-};
+const DonateRecyclables: React.FC = () => (
+  <Container className="my-5">
+    <Card className="shadow-lg p-4">
+      <Card.Body className="text-center">
+        <Card.Title>
+          <h2 className="heading heading-2">Donate Recyclables</h2>
+        </Card.Title>
+        <Card.Text>Your contributions help us make a difference!</Card.Text>
+        <Card.Img
+          variant="top"
+          src={recyclablesImage}
+          alt="Items for Good Causes"
+          className="img-fluid my-4"
+          style={{ maxHeight: "900px", objectFit: "contain" }}
+        />
+        <Card.Text>
+          You can help us raise money by recycling any of the items shown below
+          at the HUB.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </Container>
+);
 
 export default DonateRecyclables;
