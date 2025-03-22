4cc5632eb73d27e80d120cbc24a49d1fc9edaaa9
David Topping <71976336+davidtopping02@users.noreply.github.com>
2025-03-22 13:10:21 +0000

    fix build errors

--- src/Pages/OpeningHours/OpeningHours.tsx
+++ src/Pages/OpeningHours/OpeningHours.tsx
@@ -1,7 +1,5 @@
-import { Container, Row, Col, Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Banner from "../../Components/Banner/Banner";
-import StyledButton from "../../Components/StyledButton/StyledButton";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 function OpeningHours() {
